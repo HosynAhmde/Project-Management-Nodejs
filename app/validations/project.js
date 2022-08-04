77912ae9d5ca49767697ef0f4cfdd91ffcd365ab
Hossein Ahmadi <hosynahmde@gmail.com>
2022-08-04 18:46:53 +0430

    change the project validation

--- app/validations/project.js
+++ app/validations/project.js
@@ -4,8 +4,8 @@ function createProjectValidator() {
     body("title").notEmpty().withMessage(" عنوان پروژه نمیتوتند خالی باشد"),
     body("text").notEmpty().withMessage("عنوان پروژه نمیتواند خالی باشد."),
     body("tags")
-      .isArray({ min: 0, max: 5 })
-      .withMessage("حداکثر استفاده از هشتگ ها ۱۰ عدد است."),
+      .isArray({ min: 0, max: 4 })
+      .withMessage("حداکثر استفاده از هشتگ ها 4 عدد است."),
   ];
 }
 module.exports = { createProjectValidator };
