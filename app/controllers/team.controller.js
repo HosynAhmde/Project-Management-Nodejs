30b0d93616f059b6a084c6ad63889d5a9ea7face
Hossein Ahmadi <hosynahmde@gmail.com>
2022-08-05 18:49:20 +0430

    complete the delete an get team controller

--- app/controllers/team.controller.js
+++ app/controllers/team.controller.js
@@ -20,7 +20,7 @@ class TeamController {
       next(error);
     }
   }
-  async getTeam(req, res, next) {
+  async getTeams(req, res, next) {
     try {
       const team = await TeamModel.find({});
       res.send({
@@ -30,8 +30,54 @@ class TeamController {
       next(error);
     }
   }
+  async getTeamById(req, res, next) {
+    try {
+      const teamID = req.params.id;
+      const team = await TeamModel.findById(teamID);
+      if (!team) throw { status: 404, message: "تیمی یافت نشد." };
+      return res.send({
+        status: 200,
+        succes: true,
+        team,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getMyTeams(req, res, next) {
+    try {
+      const userID = req.user._id;
+      const team = await TeamModel.find({
+        $or: [{ owner: userID }, { users: userID }],
+      });
+      return res.send({
+        status: 200,
+        succes: true,
+        team,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+  async removeTeam(req, res, next) {
+    try {
+      const teamID = req.params.id;
+      const team = await TeamModel.findById(teamID);
+      if (!team) throw { status: 404, message: "تیمی یافت نشد." };
+      const result = await TeamModel.deleteOne({ _id: teamID });
+      if (result.deletedCount == 0)
+        throw { status: 400, message: "حذف تیم انجام نشد" };
+      return res.send({
+        message: "حذف با موفقیت انجام شد.",
+        status: 200,
+        succes: true,
+      });
+    } catch (error) {
+      next();
+    }
+  }
+
   inviteUserToTeam() {}
-  removeTeam() {}
   updateTeam() {}
   removeUserFromTeam() {}
 }
--- app/routers/team.js
+++ app/routers/team.js
@@ -4,6 +4,7 @@ const chekLogin = require("../middleware/autoLogin");
 const { expressValidatorErrors } = require("../middleware/validatorErrors");
 const { TeamController } = require("../controllers/team.controller");
 const createTeamValidator = require("../validations/team");
+const idValidation = require("../validations/id");
 router.post(
   "/create",
   chekLogin,
@@ -12,6 +13,21 @@ router.post(
   TeamController.createTeam
 );
 
-router.get("/list", chekLogin, TeamController.getTeam);
+router.get("/list", chekLogin, TeamController.getTeams);
+router.get("/me", chekLogin, TeamController.getMyTeams);
 
+router.get(
+  "/:id",
+  chekLogin,
+  idValidation(),
+  expressValidatorErrors,
+  TeamController.getTeamById
+);
+router.delete(
+  "/:id",
+  chekLogin,
+  idValidation(),
+  expressValidatorErrors,
+  TeamController.removeTeam
+);
 module.exports = { teamRoutes: router };
